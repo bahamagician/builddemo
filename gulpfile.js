dc0c89214e8f4353604b999022298b92a632fe9c
Buttaz <cthronebury@gmail.com>
2015-12-08 16:39:06 -0500

    Refactored Build Process

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
@@ -14,29 +14,29 @@ var browserSync = require('browser-sync').create();
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('assets/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 
 // Compile Our Sass
-gulp.task('sass', function() {    
+gulp.task('sass', function() {
     return gulp.src('assets/styles/*.scss')
         .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('_css'))
+        .pipe(gulp.dest('dist/_css'))
         .pipe(browserSync.stream())
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('_css'))
-        .pipe(browserSync.stream())        
+        .pipe(gulp.dest('dist/_css'))
+        .pipe(browserSync.stream())
         .pipe(notify({
             message: "SASS Compiled"
-        }));        
+        }));
 });
 
 // Concatenate & Minify JS
@@ -46,27 +46,40 @@ gulp.task('scripts', function() {
         'assets/js/*.js'
         ])
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('_js'))
+        .pipe(gulp.dest('dist/_js'))
         .pipe(rename('all.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('_js'))
+        .pipe(gulp.dest('dist/_js'))
         .pipe(notify({
             message: "JS Compiled"
         }));
 });
+
+gulp.task('html', function(){
+    return gulp.src([
+        'pages/*.html'
+    ])
+    .pipe(gulp.dest('dist'));
+});
 gulp.task('reload', function(){
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./dist"
         }
     });
 });
 // Watch Files For Changes
 gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
     gulp.watch('assets/js/*.js', ['lint', 'scripts', 'reload']);
     gulp.watch('assets/styles/*.scss', ['sass', 'reload']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch('pages/*.html',['html']);
+    gulp.watch("pages/*.html").on('change', browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'reload']);
+gulp.task('default', ['lint', 'sass', 'scripts', 'html']);
